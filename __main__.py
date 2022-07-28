45292b8924324362f2b264601e3ecc053d005d33
Amanda Moore <amanda.joy24@outlook.com>
2022-07-28 05:57:25 -0700

    Report database ips to individual services

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+from typing import List
 import pulumi
 import vpc
 import tls
@@ -20,6 +21,28 @@ key = Key("key", "key.pub", "key.openssh")
 main_vpc = vpc.VirtualPrivateCloud("main_vpc", key)
 main_rqlite = rqlite.RqliteCluster("main_rqlite", main_vpc, id_offset=rqlite_id_offset)
 main_redis = redis.RedisCluster("main_redis", main_vpc)
+
+
+def make_standard_webapp_configuration(args) -> str:
+    rqlite_ips: List[str] = args[: len(main_rqlite.instances)]
+    redis_ips: List[str] = args[len(rqlite_ips) :]
+
+    joined_rqlite_ips = ",".join(rqlite_ips)
+    joined_redis_ips = ",".join(redis_ips)
+
+    return "\n".join(
+        [
+            f'export RQLITE_IPS="{joined_rqlite_ips}"',
+            f'export REDIS_IPS="{joined_redis_ips}"',
+        ]
+    )
+
+
+standard_configuration = pulumi.Output.all(
+    *[instance.private_ip for instance in main_rqlite.instances],
+    *[instance.private_ip for instance in main_redis.instances],
+).apply(make_standard_webapp_configuration)
+
 backend_rest = webapp.Webapp(
     "backend_rest",
     main_vpc,
@@ -28,6 +51,7 @@ backend_rest = webapp.Webapp(
     github_pat,
     main_vpc.bastion.public_ip,
     key,
+    configuration=standard_configuration,
 )
 backend_ws = webapp.Webapp(
     "backend_ws",
@@ -37,6 +61,7 @@ backend_ws = webapp.Webapp(
     github_pat,
     main_vpc.bastion.public_ip,
     key,
+    configuration=standard_configuration,
 )
 frontend = webapp.Webapp(
     "frontend",
@@ -46,6 +71,7 @@ frontend = webapp.Webapp(
     github_pat,
     main_vpc.bastion.public_ip,
     key,
+    configuration=standard_configuration,
 )
 main_reverse_proxy = reverse_proxy.ReverseProxy(
     "main_reverse_proxy", main_vpc, key, backend_rest, backend_ws, frontend
